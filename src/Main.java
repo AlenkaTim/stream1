b9db4d675c24f7ec382968672d44c8673bea2e57
ok boss <weboptipro@yandex.ru>
2023-08-12 17:20:30 +0300

    two

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,32 @@
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
+import java.util.Collections;
 import java.util.List;
-import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
         List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
-        Stream<Integer> stream = intList.stream();
-        intList.stream()
-                .filter(x -> x > 0)
-                .filter(x -> x % 2 == 0)
-                .sorted(Comparator.naturalOrder())
-                .forEach(System.out::println);
+
+        ArrayList<Integer> positiveNumbers = new ArrayList<>();
+        for (int num : intList) {
+            if (num > 0) {
+                positiveNumbers.add(num);
+            }
+        }
+
+        ArrayList<Integer> evenNumbers = new ArrayList<>();
+        for (int num : positiveNumbers) {
+            if (num % 2 == 0) {
+                evenNumbers.add(num);
+            }
+        }
+
+        Collections.sort(evenNumbers);
+
+        for (int num : evenNumbers) {
+            System.out.println(num);
+        }
+
 
     }
 }
